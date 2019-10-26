70451c8e372325b3f94d79f12031930e975eb422
Gary Schwaeber <gschwaeb@gmail.com>
2019-10-26 16:53:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,17 +2,18 @@ class Dog
   def initalize(name)
     @name=name
   end
-
-def name=(new_name)
-  @name=new_name
-end
-
   def name
     @name
   end
 
+def name=(new_name)
+  @name = new_name
+end
+
+
+
 def breed=(new_breed)
-  new_breed=@breed
+  @breed = new_breed
 end
 
 def breed
